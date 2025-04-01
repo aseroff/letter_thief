5e5f5bd92b763e56cd3853fa28428fef1a333c52
aseroff <andy@seroff.co>
2025-04-01 12:50:49 -0500

    ignore CSP

--- app/controllers/letter_thief/application_controller.rb
+++ app/controllers/letter_thief/application_controller.rb
@@ -1,4 +1,7 @@
 module LetterThief
   class ApplicationController < ActionController::Base
+    content_security_policy do |policy|
+      policy.style_src_elem :self, :unsafe_inline
+    end
   end
 end
