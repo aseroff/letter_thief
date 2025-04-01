38cff13992cf94bdd32a4a08540300eb49d1ad44
aseroff <andy@seroff.co>
2025-04-01 12:59:11 -0500

    Update application_controller.rb

--- app/controllers/letter_thief/application_controller.rb
+++ app/controllers/letter_thief/application_controller.rb
@@ -1,7 +1,8 @@
 module LetterThief
   class ApplicationController < ActionController::Base
     content_security_policy do |policy|
-      policy.style_src :self, :unsafe_inline, 'cdn.jsdelivr.net'
+      policy.script_src  :self, :unsafe_inline
+      policy.style_src   :self, :unsafe_inline, 'cdn.jsdelivr.net'
     end
   end
 end
