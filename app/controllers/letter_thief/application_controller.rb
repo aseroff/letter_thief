441a5c1e8d1ebd97d4d06eff01082ad709e3de8f
aseroff <andy@seroff.co>
2025-04-01 13:01:51 -0500

    Update application_controller.rb

--- app/controllers/letter_thief/application_controller.rb
+++ app/controllers/letter_thief/application_controller.rb
@@ -1,8 +1,15 @@
 module LetterThief
   class ApplicationController < ActionController::Base
+    before_action :turn_off_csp_nonce_generation
     content_security_policy do |policy|
       policy.script_src  :self, :unsafe_inline
       policy.style_src   :self, :unsafe_inline, 'cdn.jsdelivr.net'
+    end
+
+    private
+
+    def turn_off_csp_nonce_generation
+      request.content_security_policy_nonce_directives = []
     end
   end
 end
